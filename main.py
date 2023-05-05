192519937d055cad8e0c6016370a021e93c386d4
Mahtot Mekonen <mahtotmkonn21@gmail.com>
2023-05-05 17:07:22 +0300

    init automation

--- main.py
+++ main.py
@@ -21,36 +21,45 @@ controller_root = r'C:\Users\mahto\Desktop\test'
 mapping_file_path = r'C:\Users\mahto\Desktop\test\MappingProfile.cs'
 
 # create_irepository_root_path = r"C:\Users\mahto\OneDrive\Documents\MAIN LAB\V7\CNET_V7_Repository.Contracts"
+
 # create_iservice_root_path = r"C:\Users\mahto\OneDrive\Documents\MAIN LAB\V7\CNET_V7_Service.Contracts"
-#
+
 # create_dto_root_path = r'C:\Users\mahto\OneDrive\Documents\MAIN LAB\V7\CNET_V7_Domain\Domain'
-#
+
 # irepository_manager_create_path = r'C:\Users\mahto\OneDrive\Documents\MAIN LAB\V7\CNET_V7_Repository.Contracts\IRepositoryManager.cs'
+
 # repository_manager_create_path = r'C:\Users\mahto\OneDrive\Documents\MAIN LAB\V7\CNET_V7_Repository.Implementation\RepositoryManager.cs'
-#
+
 # iservice_manager_create_path = r'C:\Users\mahto\OneDrive\Documents\MAIN LAB\V7\CNET_V7_Service.Contracts\IServiceManager.cs'
+
 # service_manager_create_path = r'C:\Users\mahto\OneDrive\Documents\MAIN LAB\V7\CNET_V7_Service.Implementation\ServiceManager.cs'
-#
+
 # create_irepository_implementation_root = r'C:\Users\mahto\OneDrive\Documents\MAIN LAB\V7\CNET_V7_Repository.Implementation'
+
 # create_iservice_implementation_root = r'C:\Users\mahto\OneDrive\Documents\MAIN LAB\V7\CNET_V7_Service.Implementation'
 
 # controller_root = r'C:\Users\mahto\OneDrive\Documents\MAIN LAB\V7\CNET_V7_Presentation\BaseControllers'
 
 if __name__ == '__main__':
+
     # create_dto(model_path, create_dto_root_path)
-    #
+    
     # create_irepositories(model_path, create_irepository_root_path)
+
     # create_irepository_implementation(model_path, create_irepository_implementation_root)
 
     # create_irepository_manager(model_path, irepository_manager_create_path)
+
     # create_repository_manager(model_path, repository_manager_create_path)
 
     # create_iservice(model_path, create_iservice_root_path)
+
     create_iservice_implementation(model_path, create_iservice_implementation_root)
 
     # create_iservice_manager(model_path, iservice_manager_create_path)
+
     # create_service_manager(model_path, service_manager_create_path)
-    #
+    
     # create_controllers(model_path, controller_root)
-    #
+    
     # configure_mapping(model_path, mapping_file_path)
